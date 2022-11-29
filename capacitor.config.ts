0a2f71f03cb6d49d34664d52bc2d394bd63e9956
Ramiro Iglesias <ramiro.igmun@gmail.com>
2022-11-29 08:11:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'get-your-route-mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
